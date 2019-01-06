32f9607a69dd1676459cbd17e60b22c7ee1c9677
0xEAB <desisma@heidel.beer>
2019-01-06 19:48:00 +0100

    Update license header to 2019

--- source/app.d
+++ source/app.d
@@ -1,5 +1,5 @@
 /+
-              Copyright 0xEAB 2016 - 2018.
+              Copyright 0xEAB 2016 - 2019.
      Distributed under the Boost Software License, Version 1.0.
         (See accompanying file LICENSE_1_0.txt or copy at
               https://www.boost.org/LICENSE_1_0.txt)
